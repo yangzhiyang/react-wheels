26aa90ec495fc79b1cc88d4b2447c5d8909e7dac
冬霖 <donglin@shouxin168.com>
2019-05-07 16:47:04 +0800

    拆分开发、生产环境 webpack 配置

--- lib/index.tsx
+++ lib/index.tsx
@@ -1,7 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import Button from './button';
 
 ReactDOM.render(
-  <h1>Hello, world!</h1>,
+  <Button />,
   document.getElementById('root')
 )
--- webpack.config.js
+++ webpack.config.js
@@ -2,9 +2,12 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-  mode: 'production',
+  // mode: 'production',
   entry: {
     index: './lib/index.tsx'
+  },
+  resolve: {
+    extensions: ['.ts', '.tsx', '.js', '.jsx']
   },
   output: {
     path: path.resolve(__dirname, 'dist/lib'),
@@ -18,11 +21,25 @@ module.exports = {
         loader: 'awesome-typescript-loader'
       }
     ]
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      title: 'React-wheels',
-      template: 'index.html'
-    })
-  ]
+  }
+  // plugins: [
+  //   new HtmlWebpackPlugin({
+  //     title: 'React-wheels',
+  //     template: 'index.html'
+  //   })
+  // ],
+  // externals: {
+  //   react: {
+  //     commonjs: 'react',
+  //     commonjs2: 'react',
+  //     amd: 'react',
+  //     root: 'React',
+  //   },
+  //   'react-dom': {
+  //     commonjs: 'react-dom',
+  //     commonjs2: 'react-dom',
+  //     amd: 'react-dom',
+  //     root: 'ReactDOM',
+  //   },
+  // },
 }
