e2d81325bfdeab920f2594de6b15eed69160710a
冬霖 <donglin@shouxin168.com>
2019-05-13 17:20:37 +0800

    feat: 发布 npm 包 0.0.1

--- lib/index.tsx
+++ lib/index.tsx
@@ -1,15 +1,1 @@
-import React from 'react';
-import ReactDom from 'react-dom';
-import Icon from './Icon';
-
-const fn: React.MouseEventHandler = (e) => {
-  console.log(e.target);
-}
-
-ReactDom.render(
-  <div>
-    <Icon name="alipay" onClick={fn} onMouseEnter={() => {console.log('enter')}} />
-    <Icon name="wechat" onClick={fn} />
-    <Icon name="qq" onClick={fn} />
-  </div>,
-  document.querySelector('#root'));
+export { default as Icon } from './Icon';
