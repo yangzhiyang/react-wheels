8c14e5edfe23dc572b4adf9f2d05f5067c549dfd
冬霖 <zhiyang_yang@126.com>
2019-11-14 23:26:13 +0800

    feat: 增加icon类型

--- lib/Icon/index.tsx
+++ lib/Icon/index.tsx
@@ -1,21 +1,22 @@
-import React from 'react';
-import classes from '../helpers/classes';
-import './importIcons';
-import './icon.scss';
+import React from "react";
+import classes from "../helpers/classes";
+import "./importIcons";
+import "./icon.scss";
 
 interface IconProps extends React.SVGAttributes<SVGElement> {
   name: string;
 }
 
-const Icon: React.FunctionComponent<IconProps> = ({className, name, ...restProps}) => {
-  return ( 
-    <svg  
-      className={classes('wheels-icon', className)}
-      {...restProps}
-    >
+const Icon: React.FunctionComponent<IconProps> = ({
+  className,
+  name,
+  ...restProps
+}) => {
+  return (
+    <svg className={classes("wheels-icon", className)} {...restProps}>
       <use xlinkHref={`#${name}`}></use>
     </svg>
   );
-}
+};
 
 export default Icon;
