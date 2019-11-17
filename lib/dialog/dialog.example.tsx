14ac30a779ebab6b212c7b4f569560b6cb6ef651
冬霖 <zhiyang_yang@126.com>
2019-11-17 23:17:51 +0800

    feat: 增加dialog事件处理

--- lib/dialog/dialog.example.tsx
+++ lib/dialog/dialog.example.tsx
@@ -6,7 +6,14 @@ export default function() {
   return (
     <div>
       <button onClick={() => setVisible(!visible)}>点我</button>
-      <Dialog visible={visible}>
+      <Dialog
+        visible={visible}
+        buttons={[
+          <button onClick={() => setVisible(false)}>1</button>,
+          <button onClick={() => setVisible(false)}>2</button>
+        ]}
+        onClose={() => setVisible(false)}
+      >
         <div>hi</div>
       </Dialog>
     </div>
--- lib/dialog/dialog.tsx
+++ lib/dialog/dialog.tsx
@@ -1,4 +1,4 @@
-import React, { Fragment } from "react";
+import React, { Fragment, ReactElement, MouseEventHandler } from "react";
 
 import Icon from "../icon";
 import { scopedClassMaker } from "../helpers/classes";
@@ -7,28 +7,46 @@ import "./dialog.scss";
 
 interface dialogProps {
   visible: boolean;
+  buttons: Array<ReactElement>;
+  onClose: MouseEventHandler;
+  closeOnClickMask?: boolean;
 }
 const scopedClass = scopedClassMaker("wheels-dialog");
 const sc = scopedClass;
 const Dialog: React.FunctionComponent<dialogProps> = ({
   visible,
-  children
+  children,
+  buttons,
+  onClose,
+  closeOnClickMask
 }) => {
+  const onClickClose: MouseEventHandler = e => {
+    onClose(e);
+  };
+  const onClickMask: MouseEventHandler = e => {
+    if (closeOnClickMask) {
+      onClose(e);
+    }
+  };
   return visible ? (
     <Fragment>
-      <div className={sc("mask")}></div>
+      <div className={sc("mask")} onClick={onClickMask}></div>
       <div className={sc()}>
-        <div className={sc("close")}>
+        <div className={sc("close")} onClick={onClickClose}>
           <Icon name="close" />
         </div>
         <header className={sc("header")}>提示</header>
         <main className={sc("main")}>{children}</main>
         <footer className={sc("footer")}>
-          <button>ok</button>
-          <button>cancel</button>
+          {buttons.map((button, index) =>
+            React.cloneElement(button, { key: index })
+          )}
         </footer>
       </div>
     </Fragment>
   ) : null;
+};
+Dialog.defaultProps = {
+  closeOnClickMask: false
 };
 export default Dialog;
