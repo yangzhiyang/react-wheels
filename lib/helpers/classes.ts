9b6359e2539a9a6b738bab291deca9f72b34b1aa
冬霖 <zhiyang_yang@126.com>
2019-11-20 20:56:34 +0800

    refactor(lib/helpers/classes.ts): 继续优化 scopedClassMaker

--- lib/helpers/classes.ts
+++ lib/helpers/classes.ts
@@ -10,23 +10,16 @@ interface ClassToggles {
   [key: string]: boolean;
 }
 
-function scopedClassMaker(prefix: string) {
-  return function(name: string | ClassToggles, options?: Options) {
-    const name2 =
-      typeof name === "string" || name === undefined ? { [name]: name } : name;
-    const scoped = Object.entries(name2)
-      .filter(kv => kv[1])
-      .map(kv => kv[0])
-      .map(name => [prefix, name].filter(Boolean).join("-"))
-      .join(" ");
-
-    if (options && options.extra) {
-      return [scoped, options.extra].filter(Boolean).join(" ");
-    } else {
-      return scoped;
-    }
-  };
-}
+const scopedClassMaker = (prefix: string) => (
+  name: string | ClassToggles,
+  options?: Options
+) =>
+  Object.entries(name instanceof Object ? name : { [name]: name })
+    .filter(kv => kv[1])
+    .map(kv => kv[0])
+    .map(name => [prefix, name].filter(Boolean).join("-"))
+    .concat((options && options.extra) || [])
+    .join(" ");
 
 export default classes;
 export { scopedClassMaker };
