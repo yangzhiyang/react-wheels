a1842fb96b398756f935bbb8b1cc48e7f19b3c70
冬霖 <zhiyang_yang@126.com>
2019-11-20 23:39:57 +0800

    feat: 美化示例页

--- example.tsx
+++ example.tsx
@@ -1,44 +1,50 @@
 import React from "react";
 import ReactDom from "react-dom";
-import { HashRouter as Router, Route, Link } from "react-router-dom";
+import { HashRouter as Router, Route, NavLink } from "react-router-dom";
 
+import { Layout, Header, Content, Footer, Aside } from "./lib/layout/layout";
 import IconExample from "./lib/icon/icon.example";
 import ButtonExample from "./lib/button/button.example";
 import DialogExample from "./lib/dialog/dialog.example";
 import LayoutExample from "./lib/layout/layout.example";
 
+import "./example.scss";
+
 const page = (
   <Router>
-    <div>
-      <header>
-        <div className="log">Wheels</div>
-      </header>
-      <div>
-        <aside>
+    <Layout className="site-page">
+      <Header className="site-header">
+        <div className="logo">
+          <span>Wheels</span>
+        </div>
+      </Header>
+      <Layout>
+        <Aside className="site-aside">
           <h2>组件</h2>
           <ul>
             <li>
-              <Link to="/icon">Icon</Link>
+              <NavLink to="/icon">Icon</NavLink>
+            </li>
+            {/* <li>
+              <NavLink to="/button">Button</NavLink>
+            </li> */}
+            <li>
+              <NavLink to="/dialog">Dialog</NavLink>
             </li>
             <li>
-              <Link to="/button">Button</Link>
-            </li>
-            <li>
-              <Link to="/dialog">Dialog</Link>
-            </li>
-            <li>
-              <Link to="/layout">Layout</Link>
+              <NavLink to="/layout">Layout</NavLink>
             </li>
           </ul>
-        </aside>
-        <main>
+        </Aside>
+        <Content className="site-main">
           <Route path="/icon" component={IconExample}></Route>
           <Route path="/button" component={ButtonExample}></Route>
           <Route path="/dialog" component={DialogExample}></Route>
           <Route path="/layout" component={LayoutExample}></Route>
-        </main>
-      </div>
-    </div>
+        </Content>
+      </Layout>
+      <Footer className="site-footer">@冬霖yzy</Footer>
+    </Layout>
   </Router>
 );
 
--- lib/layout/layout.tsx
+++ lib/layout/layout.tsx
@@ -28,3 +28,8 @@ const Layout: React.FunctionComponent<Props> = ({
 };
 
 export default Layout;
+export { Layout };
+export { default as Header } from "./header";
+export { default as Content } from "./content";
+export { default as Footer } from "./footer";
+export { default as Aside } from "./aside";
