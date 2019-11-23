7142b6907632a33ebdf996ae610da5dd86ce6f25
冬霖 <zhiyang_yang@126.com>
2019-11-23 22:41:51 +0800

    feat: 优化校验方法

--- lib/form/validator.ts
+++ lib/form/validator.ts
@@ -21,25 +21,17 @@ const Validator = (formData: FormValue, formRules: FormRules): Errors => {
   };
   formRules.map(rule => {
     const value = formData[rule.key];
-    if (rule.required) {
-      if (isEmpty(value)) {
-        addError(rule.key, `${rule.key}不能为空`);
-      }
+    if (rule.required && isEmpty(value)) {
+      addError(rule.key, `${rule.key}不能为空`);
     }
-    if (rule.minLength) {
-      if (!isEmpty(value) && value.length < rule.minLength) {
-        addError(rule.key, `${rule.key}长度不得小于${rule.minLength}`);
-      }
+    if (rule.minLength && !isEmpty(value) && value.length < rule.minLength) {
+      addError(rule.key, `${rule.key}长度不得小于${rule.minLength}`);
     }
-    if (rule.maxLength) {
-      if (!isEmpty(value) && value.length > rule.maxLength) {
-        addError(rule.key, `${rule.key}长度不得超过${rule.maxLength}`);
-      }
+    if (rule.maxLength && !isEmpty(value) && value.length > rule.maxLength) {
+      addError(rule.key, `${rule.key}长度不得超过${rule.maxLength}`);
     }
-    if (rule.pattern) {
-      if (!rule.pattern.test(value)) {
-        addError(rule.key, `${rule.key}格式不正确`);
-      }
+    if (rule.pattern && !rule.pattern.test(value)) {
+      addError(rule.key, `${rule.key}格式不正确`);
     }
   });
   return errors;
